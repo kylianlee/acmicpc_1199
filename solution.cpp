e1b0be5ec312f6371f33a5ee97ce1eca9645532b
Kylian <hsjgc1234@gmail.com>
2021-09-19 17:07:40 +0900

    prototype solution

--- solution.cpp
+++ solution.cpp
@@ -4,79 +4,70 @@
 
 #include <iostream>
 #include <vector>
-#include <queue>
-
+#define SIZE 1001
 using namespace std;
 
-vector<vector<int>> graph;
-vector<vector<queue<int>>> circuit_list;
+//vector<vector<int>> graph;
+//vector<int> node;
+int graph[SIZE][SIZE];
+int node[SIZE];
+int node_num;
 
-void findCircuit(int next, int orig, queue<int> q);
-bool isEulerianCircuit();
+void dfs(int prev);
 
 int main(){
-  int size;
-  cin >> size;
-  graph.resize(size);
-  circuit_list.resize(size);
-  for (int i = 0; i < size; ++i) {
-    graph[i].resize(size);
-  }
-  for (int i = 0; i < size; ++i) {
-    for (int j = 0; j < size; ++j) {
-      cin >> graph[i][j];
-    }
-  }
-  if(isEulerianCircuit()){
-    for (int i = 0; i < size; ++i) {
-      for (int j = 0; j < size; ++j) {
-        if(graph[i][j] != 0){
-          graph[i][j]--;
-          queue<int> q;
-          q.push(i);
-          findCircuit(j, i, q);
-        }
-      }
-    }
-  }
-  else
-    cout << -1 << endl;
-  return 0;
-}
-
-void findCircuit(int next, int orig, queue<int> q){
-  q.push(next);
-  int size = graph.size();
-  for (int i = 0; i < size; ++i) {
-    if(graph[next][i] != 0){
-      graph[next][i]--;
-      if(i == orig){
-        q.push(i);
-        circuit_list[orig].push_back(q);
-
-        break;
-      }
-      else{
-        findCircuit(i, orig, q);
-      }
-    }
-  }
-}
-
-bool isEulerianCircuit(){
-  bool flag = true;
-  int size = graph.size();
-  for (int i = 0; i < size; ++i) {
-    int count = 0;
-    for (int j = 0; j < size; ++j) {
+//  int node_num;
+//  cin >> node_num;
+  scanf("%d", &node_num);
+//  graph.resize(node_num);
+//  node.resize(node_num);
+//  for (int i = 0; i < node_num; ++i) {
+//    graph[i].resize(node_num);
+//  }
+  for (int i = 0; i < node_num; ++i) {
+    for (int j = 0; j < node_num; ++j) {
+//      cin >> graph[i][j];
+      scanf("%d", &graph[i][j]);
       if(graph[i][j] != 0){
-        count++;
+        node[i] += graph[i][j];
+        node[j] += graph[i][j];
       }
     }
-    if(count % 2 != 0){
-      flag = false;
+  }
+  bool is_eulerian = true;
+  for (int i = 0; i < node_num; ++i) {
+    node[i] /= 2;
+    if(node[i] % 2) {
+      is_eulerian = false;
       break;
     }
   }
-  return flag;
+  if(is_eulerian){
+    dfs(0);
+  }
+  else{
+//    cout << -1 << endl;
+    printf("%d\n", -1);
+  }
+  return 0;
+}
+
+void dfs(int prev){
+  if(!node[prev]){
+    printf("%d ", prev + 1);
+    return;
+  }
+//  int size = graph.size();
+  int size = node_num;
+  for (int i = 0; i < size; ++i) {
+    if(graph[prev][i] != 0){
+      graph[prev][i]--;
+      graph[i][prev]--;
+      node[i]--;
+      node[prev]--;
+      dfs(i);
+    }
+  }
+//  cout << prev + 1 << ' ';
+  printf("%d ", prev + 1);
 }
