17f93f7592fae9070057f5d0a09843c437800311
Kylian <hsjgc1234@gmail.com>
2021-09-21 19:49:38 +0900

    modified

--- solution.cpp
+++ solution.cpp
@@ -2,31 +2,29 @@
 // Created by Kylian Lee on 2021/08/16.
 //
 
-#include <iostream>
 #include <vector>
+#include <utility>
 #define SIZE 1001
 using namespace std;
 
-//vector<vector<int>> graph;
-//vector<int> node;
-int graph[SIZE][SIZE];
-int node[SIZE];
+vector<vector<int>> graph;
+vector<int> node;
+//int graph[SIZE][SIZE];
+//int node[SIZE];
 int node_num;
 
 void dfs(int prev);
 
 int main(){
-//  int node_num;
 //  cin >> node_num;
   scanf("%d", &node_num);
-//  graph.resize(node_num);
-//  node.resize(node_num);
-//  for (int i = 0; i < node_num; ++i) {
-//    graph[i].resize(node_num);
-//  }
+  graph.resize(node_num);
+  node.resize(node_num);
+  for (int i = 0; i < node_num; ++i) {
+    graph[i].resize(node_num);
+  }
   for (int i = 0; i < node_num; ++i) {
     for (int j = 0; j < node_num; ++j) {
-//      cin >> graph[i][j];
       scanf("%d", &graph[i][j]);
       if(graph[i][j] != 0){
         node[i] += graph[i][j];
@@ -46,7 +44,6 @@ int main(){
     dfs(0);
   }
   else{
-//    cout << -1 << endl;
     printf("%d\n", -1);
   }
   return 0;
@@ -57,7 +54,6 @@ void dfs(int prev){
     printf("%d ", prev + 1);
     return;
   }
-//  int size = graph.size();
   int size = node_num;
   for (int i = 0; i < size; ++i) {
     if(graph[prev][i] != 0){
@@ -68,6 +64,5 @@ void dfs(int prev){
       dfs(i);
     }
   }
-//  cout << prev + 1 << ' ';
   printf("%d ", prev + 1);
 }
